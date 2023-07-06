e589b8d0c93a2327a995b785a3e777634f1de04a
Your Name <you@example.com>
2023-07-07 00:04:01 +0200

    style

--- var/twig/80/8050464f650fedaf4c0eca98a6e4e1a3.php
+++ var/twig/80/8050464f650fedaf4c0eca98a6e4e1a3.php
@@ -34,33 +34,116 @@ class __TwigTemplate_fc4031e373bdc003037f1d86dbae2e33 extends Template
     {
         $macros = $this->macros;
         // line 1
-        echo "<form action=\"/showMusic\" method=\"post\">
-  <input type=\"text\" name=\"textInput\"/>
-  <input type=\"submit\" value=\"Rechercher\" name=\"submitInput\"/><br>
-</form>
-<h1>Résultats de recherche :</h1>
-";
-        // line 6
+        echo "<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+  font-family: Arial, sans-serif;
+  background-color: #f4f4f4;
+}
+
+.container {
+  width: 80%;
+  margin: auto;
+  overflow: hidden;
+}
+
+form {
+  margin: 30px 0;
+  display: flex;
+  justify-content: center;
+}
+
+form input[type=\"text\"] {
+  padding: 10px;
+  font-size: 17px;
+  border: 1px solid grey;
+  flex: 9;
+}
+
+form input[type=\"submit\"] {
+  flex: 1;
+  padding: 10px;
+  font-size: 17px;
+  background-color: #1db954;
+  color: white;
+  border: 1px solid grey;
+  cursor: pointer;
+}
+
+form input[type=\"submit\"]:hover {
+  background-color: #1ed760;
+}
+
+h1 {
+  text-align: center;
+  color: #333;
+}
+
+.track {
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+.track:nth-child(even) {
+  flex-direction: row-reverse;
+}
+
+.track img {
+  width: 100px;
+  height: 100px;
+  margin-right: 20px;
+}
+
+.track h2, .track h3 {
+  margin: 0;
+}
+
+.track h3 {
+  color: #666;
+}
+</style>
+</head>
+<body>
+<div class=\"container\">
+  <form action=\"/showMusic\" method=\"post\">
+    <input type=\"text\" name=\"textInput\"/>
+    <input type=\"submit\" value=\"Rechercher\" name=\"submitInput\"/>
+  </form>
+  <h1>Résultats de recherche :</h1>
+  ";
+        // line 80
         $context['_parent'] = $context;
-        $context['_seq'] = twig_ensure_traversable(twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, ($context["listemusique"] ?? null), "tracks", [], "any", false, false, false, 6), "items", [], "any", false, false, false, 6));
+        $context['_seq'] = twig_ensure_traversable(twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, ($context["listemusique"] ?? null), "tracks", [], "any", false, false, false, 80), "items", [], "any", false, false, false, 80));
         foreach ($context['_seq'] as $context["_key"] => $context["track"]) {
-            // line 7
-            echo "  <h2>";
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["track"], "name", [], "any", false, false, false, 7), "html", null, true);
-            echo "</h2>
-  <h3>";
-            // line 8
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (($__internal_compile_0 = twig_get_attribute($this->env, $this->source, $context["track"], "artists", [], "any", false, false, false, 8)) && is_array($__internal_compile_0) || $__internal_compile_0 instanceof ArrayAccess ? ($__internal_compile_0[0] ?? null) : null), "name", [], "any", false, false, false, 8), "html", null, true);
-            echo "</h3>
-  <img src=\"";
-            // line 9
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (($__internal_compile_1 = twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, $context["track"], "album", [], "any", false, false, false, 9), "images", [], "any", false, false, false, 9)) && is_array($__internal_compile_1) || $__internal_compile_1 instanceof ArrayAccess ? ($__internal_compile_1[2] ?? null) : null), "url", [], "any", false, false, false, 9), "html", null, true);
+            // line 81
+            echo "    <div class=\"track\">
+      <img src=\"";
+            // line 82
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (($__internal_compile_0 = twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, $context["track"], "album", [], "any", false, false, false, 82), "images", [], "any", false, false, false, 82)) && is_array($__internal_compile_0) || $__internal_compile_0 instanceof ArrayAccess ? ($__internal_compile_0[2] ?? null) : null), "url", [], "any", false, false, false, 82), "html", null, true);
             echo "\">
-";
+      <div>
+        <h2>";
+            // line 84
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["track"], "name", [], "any", false, false, false, 84), "html", null, true);
+            echo "</h2>
+        <h3>";
+            // line 85
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (($__internal_compile_1 = twig_get_attribute($this->env, $this->source, $context["track"], "artists", [], "any", false, false, false, 85)) && is_array($__internal_compile_1) || $__internal_compile_1 instanceof ArrayAccess ? ($__internal_compile_1[0] ?? null) : null), "name", [], "any", false, false, false, 85), "html", null, true);
+            echo "</h3>
+      </div>
+    </div>
+  ";
         }
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_iterated'], $context['_key'], $context['track'], $context['_parent'], $context['loop']);
         $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 89
+        echo "</div>
+</body>
+</html>";
     }
 
     public function getTemplateName()
@@ -75,20 +158,101 @@ class __TwigTemplate_fc4031e373bdc003037f1d86dbae2e33 extends Template
 
     public function getDebugInfo()
     {
-        return array (  57 => 9,  53 => 8,  48 => 7,  44 => 6,  37 => 1,);
+        return array (  144 => 89,  134 => 85,  130 => 84,  125 => 82,  122 => 81,  118 => 80,  37 => 1,);
     }
 
     public function getSourceContext()
     {
-        return new Source("<form action=\"/showMusic\" method=\"post\">
-  <input type=\"text\" name=\"textInput\"/>
-  <input type=\"submit\" value=\"Rechercher\" name=\"submitInput\"/><br>
-</form>
-<h1>Résultats de recherche :</h1>
-{% for track in listemusique.tracks.items %}
-  <h2>{{ track.name }}</h2>
-  <h3>{{ track.artists[0].name }}</h3>
-  <img src=\"{{ track.album.images[2].url }}\">
-{% endfor %}", "index.html.twig", "/workspaces/Spotify/templates/index.html.twig");
+        return new Source("<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+  font-family: Arial, sans-serif;
+  background-color: #f4f4f4;
+}
+
+.container {
+  width: 80%;
+  margin: auto;
+  overflow: hidden;
+}
+
+form {
+  margin: 30px 0;
+  display: flex;
+  justify-content: center;
+}
+
+form input[type=\"text\"] {
+  padding: 10px;
+  font-size: 17px;
+  border: 1px solid grey;
+  flex: 9;
+}
+
+form input[type=\"submit\"] {
+  flex: 1;
+  padding: 10px;
+  font-size: 17px;
+  background-color: #1db954;
+  color: white;
+  border: 1px solid grey;
+  cursor: pointer;
+}
+
+form input[type=\"submit\"]:hover {
+  background-color: #1ed760;
+}
+
+h1 {
+  text-align: center;
+  color: #333;
+}
+
+.track {
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+.track:nth-child(even) {
+  flex-direction: row-reverse;
+}
+
+.track img {
+  width: 100px;
+  height: 100px;
+  margin-right: 20px;
+}
+
+.track h2, .track h3 {
+  margin: 0;
+}
+
+.track h3 {
+  color: #666;
+}
+</style>
+</head>
+<body>
+<div class=\"container\">
+  <form action=\"/showMusic\" method=\"post\">
+    <input type=\"text\" name=\"textInput\"/>
+    <input type=\"submit\" value=\"Rechercher\" name=\"submitInput\"/>
+  </form>
+  <h1>Résultats de recherche :</h1>
+  {% for track in listemusique.tracks.items %}
+    <div class=\"track\">
+      <img src=\"{{ track.album.images[2].url }}\">
+      <div>
+        <h2>{{ track.name }}</h2>
+        <h3>{{ track.artists[0].name }}</h3>
+      </div>
+    </div>
+  {% endfor %}
+</div>
+</body>
+</html>", "index.html.twig", "/workspaces/Spotify/templates/index.html.twig");
     }
 }
